d8cdefaff99c1d01bbce78fe7c13fbc5b30ddcd3
Vladyslav Vlasenko <glasierrvlas@gmail.com>
2016-07-10 02:20:08 +0300

    Repository: Add repositories for all route and schedule entities

---
+++ hermes-repository/src/main/java/eu/socialedge/hermes/repository/route/SpringJpaStationRepository.java
@@ -0,0 +1,34 @@
+/**
+ * Hermes - a Public Transport Management System
+ * Copyright (c) 2016 SocialEdge
+ * <p>
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+package eu.socialedge.hermes.repository.route;
+
+import eu.socialedge.hermes.repository.SpringJpaRepository;
+import eu.socialedge.hermes.route.Station;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+import javax.inject.Inject;
+
+@Component
+public class SpringJpaStationRepository extends SpringJpaRepository<Integer, Station> implements StationRepository{
+    @Repository
+    private interface InternalJpaRepository extends JpaRepository<Station, Integer> {}
+
+    @Inject
+    public SpringJpaStationRepository(JpaRepository<Station, Integer> internalRepository) {
+        super(internalRepository);
+    }
+}
