26edba3d1ad16f3d351d107f83c2ff4c99c968e3
Vladyslav Vlasenko <glasierrvlas@gmail.com>
2017-03-20 15:42:13 +0300

    Create google maps api based shape factory (#79)

--- backend-core/src/main/java/eu/socialedge/hermes/backend/core/ShapeFactory.java
+++ backend-core/src/main/java/eu/socialedge/hermes/backend/core/ShapeFactory.java
@@ -14,22 +14,9 @@
  */
 package eu.socialedge.hermes.backend.core;
 
-import tec.uom.se.quantity.Quantities;
+import java.util.List;
 
-import javax.measure.Quantity;
-import javax.persistence.AttributeConverter;
-import javax.persistence.Converter;
+public interface ShapeFactory {
 
-@Converter(autoApply = true)
-public class QuantityConverter implements AttributeConverter<Quantity, String> {
-
-    @Override
-    public String convertToDatabaseColumn(Quantity attribute) {
-        return attribute != null ? attribute.toString() : null;
-    }
-
-    @Override
-    public Quantity convertToEntityAttribute(String dbData) {
-        return dbData != null ? Quantities.getQuantity(dbData) : null;
-    }
+    Shape create(List<Location> points);
 }
