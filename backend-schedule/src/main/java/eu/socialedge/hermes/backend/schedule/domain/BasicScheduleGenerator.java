f4a8c4a6002539bf1eb6ec67ec6958c25300aad4
Vladyslav Vlasenko <vladyslav.vlasenko@nixsolutions.com>
2017-03-30 20:57:32 +0300

    Lombok variables a little bit, tidy code. Maybe remove lombok variables if not all variables in class are lombocked?

--- backend-schedule/src/main/java/eu/socialedge/hermes/backend/schedule/domain/BasicScheduleGenerator.java
+++ backend-schedule/src/main/java/eu/socialedge/hermes/backend/schedule/domain/BasicScheduleGenerator.java
@@ -4,7 +4,6 @@ import eu.socialedge.hermes.backend.schedule.domain.api.ScheduleGenerator;
 import eu.socialedge.hermes.backend.transit.domain.*;
 import lombok.*;
 import lombok.experimental.Accessors;
-import tec.uom.se.quantity.Quantities;
 import tec.uom.se.unit.Units;
 
 import javax.measure.Quantity;
@@ -22,24 +21,18 @@ public class BasicScheduleGenerator implements ScheduleGenerator {
 
     private @NonNull Route route;
 
+    private @NonNull String description;
+    private @NonNull Availability availability;
+
     private @NonNull LocalTime startTimeInbound;
     private @NonNull LocalTime startTimeOutbound;
     private @NonNull LocalTime endTimeInbound;
     private @NonNull LocalTime endTimeOutbound;
-    private @NonNull Duration headway; // Interval between trips
+    private @NonNull Duration headway;
     private @NonNull Integer fleetSize;
-    private @NonNull Duration dwellTime; // Time spent at one stop
+    private @NonNull Duration dwellTime;
     private @NonNull Quantity<Speed> averageSpeed;
-    private @NonNull Duration minLayover; // Time to wait at the end of the trip before going in opposite direction
-
-    private @NonNull String description;
-    private @NonNull Availability availability;
-
-    private LocalTime peakStartTime;
-    private LocalTime peakEndTime;
-    private int peakFleetIncrease;
-    private Duration peakDwellTime;
-    private Quantity<Speed> peakAverageSpeed;
+    private @NonNull Duration minLayover;
 
 //    Break?
 
@@ -51,7 +44,7 @@ public class BasicScheduleGenerator implements ScheduleGenerator {
     }
 
     private Set<Trip> magic() {
-        List<TimePoint> timePoints = new ArrayList<>();
+        val timePoints = new ArrayList<TimePoint>();
         LocalTime nextTimePoint = startTimeInbound;
         while (nextTimePoint.isBefore(endTimeInbound)) {
             timePoints.add(new TimePoint(INBOUND, nextTimePoint, false));
@@ -63,11 +56,11 @@ public class BasicScheduleGenerator implements ScheduleGenerator {
             nextTimePoint = nextTimePoint.plus(headway);
         }
 
-        Set<Trip>  trips = new HashSet<>();
+        val trips = new HashSet<Trip>();
         int vehId = 1;
         while (hasAvailableTimePoints(timePoints)) {
-            TimePoint startPoint = getNextAvailableTimePoint(timePoints);
-            List<Trip> vehicleTrips = generateVehicleTrips(vehId, startPoint, timePoints);
+            val startPoint = getNextAvailableTimePoint(timePoints);
+            val vehicleTrips = generateVehicleTrips(vehId, startPoint, timePoints);
             trips.addAll(vehicleTrips);
             vehId++;
         }
@@ -76,17 +69,17 @@ public class BasicScheduleGenerator implements ScheduleGenerator {
     }
 
     private List<Trip> generateVehicleTrips(int vehicleId, TimePoint startPoint, List<TimePoint> timePoints) {
-        List<Trip> trips = new ArrayList<>();
-        DirectionToggle direction = new DirectionToggle(startPoint.direction());
+        val trips = new ArrayList<Trip>();
+        val directionToggle = new DirectionToggle(startPoint.direction());
 
-        boolean canTravel = startPoint.time().isBefore(direction.get().equals(INBOUND) ? endTimeOutbound : endTimeInbound);
+        boolean canTravel = startPoint.time().isBefore(directionToggle.get().equals(INBOUND) ? endTimeOutbound : endTimeInbound);
         TimePoint currentPoint = startPoint;
         while (canTravel) {
-            Trip trip = genTrip(vehicleId, currentPoint);
-            direction.turn();
+            val trip = genTrip(vehicleId, currentPoint);
+            directionToggle.turn();
 
-            LocalTime currentTime = getArrivalTime(trip);
-            Optional<TimePoint> nextPoint = findNextAvailableAfterTime(timePoints, currentTime, direction.get());
+            val currentTime = getArrivalTime(trip);
+            val nextPoint = findNextAvailableAfterTime(timePoints, currentTime, directionToggle.get());
 
             if (nextPoint.isPresent()) {
                 currentPoint = nextPoint.get();
@@ -133,7 +126,7 @@ public class BasicScheduleGenerator implements ScheduleGenerator {
 
     private Trip genTrip(int vehicleId, TimePoint timePoint) {
         timePoint.isServiced(true);
-        return new Trip(timePoint.direction(), route, "headsign", calculateStopTimes(timePoint.time(), route.shape(), averageSpeed, dwellTime));
+        return new Trip(eu.socialedge.hermes.backend.transit.domain.Direction.INBOUND, route, "headsign", calculateStopTimes(timePoint.time(), route.shape(), averageSpeed, dwellTime));
     }
 
     private List<Stop> calculateStopTimes(LocalTime startTime, Shape shape, Quantity<Speed> averageSpeed, Duration dwellTime) {
@@ -143,7 +136,7 @@ public class BasicScheduleGenerator implements ScheduleGenerator {
             val distTravelled = shape.shapePoints().stream()
                 .filter(shapePoint -> station.location().equals(shapePoint.location()))
                 .findFirst()
-                .orElseThrow(() -> new ScheduleGeneratorException("Could not match passed stations with route shape. Station not found at location + " + station.location()))
+                .orElseThrow(() -> new ScheduleGeneratorException("Could not match stations with route shape. Station not found at location + " + station.location()))
                 .distanceTraveled();
 
             val distValue = distTravelled.to(Units.METRE).getValue().longValue();
@@ -155,102 +148,6 @@ public class BasicScheduleGenerator implements ScheduleGenerator {
 
         return stopTimes;
     }
-
-//TODO remove it
-    public static void main(String[] args) {
-        val station = new Station("name", new HashSet<VehicleType>() {{ add(VehicleType.BUS);}}, new Location(2.3, 3.2));
-        val route = new Route("code",
-            VehicleType.BUS,
-            Collections.singletonList(station),
-            new Shape(Collections.singletonList(new ShapePoint(new Location(2.3, 3.2), Quantities.getQuantity(140, Units.METRE)))));
-        val generator = BasicScheduleGenerator.builder()
-            .averageSpeed(Quantities.getQuantity(10, Units.METRE_PER_SECOND))
-            .dwellTime(Duration.ofMinutes(1))
-            .endTimeInbound(LocalTime.MIDNIGHT)
-            .endTimeOutbound(LocalTime.MIDNIGHT)
-            .startTimeInbound(LocalTime.NOON)
-            .startTimeOutbound(LocalTime.NOON)
-            .headway(Duration.ofHours(1))
-            .fleetSize(10)
-            .minLayover(Duration.ofMinutes(4))
-            .route(route).build();
-
-        //canTravel
-        isTrue(generator.canTravel(LocalTime.of(10, 10), new TimePoint(INBOUND, LocalTime.of(10, 15), true)));
-        isFalse(generator.canTravel(LocalTime.of(10, 15), new TimePoint(INBOUND, LocalTime.of(10, 15), true)));
-        isFalse(generator.canTravel(LocalTime.of(10, 25), new TimePoint(INBOUND, LocalTime.of(10, 15), true)));
-        isTrue(generator.canTravel(LocalTime.of(10, 11), new TimePoint(INBOUND, LocalTime.of(10, 15), true)));
-        isFalse(generator.canTravel(LocalTime.of(10, 13), new TimePoint(INBOUND, LocalTime.of(10, 15), true)));
-
-        //hasAvailableTimePoints
-        isFalse(hasAvailableTimePoints(Arrays.asList(new TimePoint(INBOUND, LocalTime.of(10, 15), true), new TimePoint(INBOUND, LocalTime.of(10, 15), true), new TimePoint(INBOUND, LocalTime.of(10, 15), true))));
-        isTrue(hasAvailableTimePoints(Arrays.asList(new TimePoint(INBOUND, LocalTime.of(10, 15), false), new TimePoint(INBOUND, LocalTime.of(10, 15), false), new TimePoint(INBOUND, LocalTime.of(10, 15), false))));
-        isTrue(hasAvailableTimePoints(Arrays.asList(new TimePoint(INBOUND, LocalTime.of(10, 15), true), new TimePoint(INBOUND, LocalTime.of(10, 15), false), new TimePoint(INBOUND, LocalTime.of(10, 15), false))));
-        isTrue(hasAvailableTimePoints(Arrays.asList(new TimePoint(INBOUND, LocalTime.of(10, 15), false), new TimePoint(INBOUND, LocalTime.of(10, 15), true), new TimePoint(INBOUND, LocalTime.of(10, 15), true))));
-
-        //getNextAvailableTimePoint
-        List<TimePoint> points = Arrays.asList(new TimePoint(INBOUND, LocalTime.of(11, 15), false), new TimePoint(INBOUND, LocalTime.of(12, 15), false), new TimePoint(INBOUND, LocalTime.of(13, 15), false));
-        isTrue(LocalTime.of(11, 15).equals(getNextAvailableTimePoint(points).time()));
-        points = Arrays.asList(new TimePoint(INBOUND, LocalTime.of(11, 15), true), new TimePoint(INBOUND, LocalTime.of(12, 15), false), new TimePoint(INBOUND, LocalTime.of(13, 15), false));
-        isTrue(LocalTime.of(12, 15).equals(getNextAvailableTimePoint(points).time()));
-        points = Arrays.asList(new TimePoint(INBOUND, LocalTime.of(11, 15), true), new TimePoint(INBOUND, LocalTime.of(12, 15), true), new TimePoint(INBOUND, LocalTime.of(13, 15), false));
-        isTrue(LocalTime.of(13, 15).equals(getNextAvailableTimePoint(points).time()));
-        points = Arrays.asList(new TimePoint(INBOUND, LocalTime.of(11, 15), true), new TimePoint(INBOUND, LocalTime.of(12, 15), true), new TimePoint(INBOUND, LocalTime.of(13, 15), true));
-        boolean thrown = false;
-        try {
-            getNextAvailableTimePoint(points);
-        } catch (RuntimeException e) {
-            thrown = true;
-        }
-        isTrue(thrown);
-
-        //getArrivalTime
-        List<Stop> stops = Arrays.asList(new Stop(LocalTime.of(10, 10), LocalTime.of(10, 11), station), new Stop(LocalTime.of(11, 10), LocalTime.of(11, 11), station), new Stop(LocalTime.of(12, 10), LocalTime.of(12, 11), station));
-        isTrue(LocalTime.of(12, 10).equals(getArrivalTime(new Trip(INBOUND, route, stops))));
-        stops = Arrays.asList(new Stop(LocalTime.of(10, 10), LocalTime.of(10, 11), station));
-        isTrue(LocalTime.of(10, 10).equals(getArrivalTime(new Trip(INBOUND, route, stops))));
-        stops = Arrays.asList(new Stop(LocalTime.of(11, 10), LocalTime.of(11, 11), station), new Stop(LocalTime.of(10, 10), LocalTime.of(10, 11), station));
-        isTrue(LocalTime.of(11, 10).equals(getArrivalTime(new Trip(INBOUND, route, stops))));
-
-        //findNearbyForService
-        points = Arrays.asList(new TimePoint(INBOUND, LocalTime.of(11, 15), false), new TimePoint(INBOUND, LocalTime.of(12, 15), false), new TimePoint(INBOUND, LocalTime.of(13, 15), false));
-        isTrue(LocalTime.of(12, 15).equals(generator.findNextAvailableAfterTime(points, LocalTime.of(11, 14), INBOUND).get().time()));
-
-        points = Arrays.asList(new TimePoint(INBOUND, LocalTime.of(11, 15), false), new TimePoint(OUTBOUND, LocalTime.of(12, 15), false), new TimePoint(INBOUND, LocalTime.of(13, 15), false));
-        isTrue(LocalTime.of(13, 15).equals(generator.findNextAvailableAfterTime(points, LocalTime.of(11, 14), INBOUND).get().time()));
-
-        points = Arrays.asList(new TimePoint(INBOUND, LocalTime.of(11, 15), false), new TimePoint(INBOUND, LocalTime.of(12, 15), false), new TimePoint(INBOUND, LocalTime.of(13, 15), false));
-        isTrue(LocalTime.of(13, 15).equals(generator.findNextAvailableAfterTime(points, LocalTime.of(12, 16), INBOUND).get().time()));
-
-        points = Arrays.asList(new TimePoint(INBOUND, LocalTime.of(11, 15), false), new TimePoint(INBOUND, LocalTime.of(12, 15), false), new TimePoint(INBOUND, LocalTime.of(13, 15), false));
-        isTrue(LocalTime.of(12, 15).equals(generator.findNextAvailableAfterTime(points, LocalTime.of(11, 15), INBOUND).get().time()));
-
-        points = Arrays.asList(new TimePoint(INBOUND, LocalTime.of(11, 15), false), new TimePoint(INBOUND, LocalTime.of(12, 15), false), new TimePoint(INBOUND, LocalTime.of(13, 15), false));
-        isTrue(LocalTime.of(11, 15).equals(generator.findNextAvailableAfterTime(points, LocalTime.of(11, 10), INBOUND).get().time()));
-
-        points = Arrays.asList(new TimePoint(OUTBOUND, LocalTime.of(11, 15), false), new TimePoint(OUTBOUND, LocalTime.of(12, 15), false), new TimePoint(INBOUND, LocalTime.of(13, 15), false));
-        isTrue(LocalTime.of(13, 15).equals(generator.findNextAvailableAfterTime(points, LocalTime.of(11, 10), INBOUND).get().time()));
-
-        points = Arrays.asList(new TimePoint(OUTBOUND, LocalTime.of(11, 15), false), new TimePoint(OUTBOUND, LocalTime.of(12, 15), false), new TimePoint(OUTBOUND, LocalTime.of(13, 15), false));
-        isTrue(!generator.findNextAvailableAfterTime(points, LocalTime.of(11, 14), INBOUND).isPresent());
-
-        points = Arrays.asList(new TimePoint(OUTBOUND, LocalTime.of(11, 15), false), new TimePoint(OUTBOUND, LocalTime.of(12, 15), false), new TimePoint(OUTBOUND, LocalTime.of(13, 15), false));
-        isTrue(!generator.findNextAvailableAfterTime(points, LocalTime.of(13, 16), OUTBOUND).isPresent());
-
-    }
-
-    private static void isTrue(boolean value) {
-        if (!value) {
-            throw new RuntimeException("Not true");
-        }
-    }
-
-    private static void isFalse(boolean value) {
-        if (value) {
-            throw new RuntimeException("Not true");
-        }
-    }
-
 }
 
 enum Direction {
--- backend-schedule/src/test/java/eu/socialedge/hermes/backend/schedule/domain/BasicScheduleGeneratorTest.java
+++ backend-schedule/src/test/java/eu/socialedge/hermes/backend/schedule/domain/BasicScheduleGeneratorTest.java
@@ -1,5 +1,41 @@
 package eu.socialedge.hermes.backend.schedule.domain;
 
+import eu.socialedge.hermes.backend.transit.domain.*;
+import lombok.val;
+import org.junit.Before;
+import tec.uom.se.quantity.Quantities;
+import tec.uom.se.unit.Units;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Collections;
+import java.util.HashSet;
+
 public class BasicScheduleGeneratorTest {
 
+    private BasicScheduleGenerator generator;
+
+    @Before
+    public void setUp() {
+            val station = new Station("name", new HashSet<VehicleType>() {{ add(VehicleType.BUS);}}, new Location(2.3, 3.2));
+            val route = new Route("code",
+                VehicleType.BUS,
+                Collections.singletonList(station),
+                new Shape(Collections.singletonList(new ShapePoint(new Location(2.3, 3.2), Quantities.getQuantity(140, Units.METRE)))));
+            generator = BasicScheduleGenerator.builder()
+                .description("description")
+                .availability(Availability.weekendDays(LocalDate.now(), LocalDate.now().plusDays(1)))
+                .averageSpeed(Quantities.getQuantity(10, Units.METRE_PER_SECOND))
+                .dwellTime(Duration.ofMinutes(1))
+                .endTimeInbound(LocalTime.MIDNIGHT)
+                .endTimeOutbound(LocalTime.MIDNIGHT)
+                .startTimeInbound(LocalTime.NOON)
+                .startTimeOutbound(LocalTime.NOON)
+                .headway(Duration.ofHours(1))
+                .fleetSize(10)
+                .minLayover(Duration.ofMinutes(4))
+                .route(route)
+                .build();
+    }
 }
