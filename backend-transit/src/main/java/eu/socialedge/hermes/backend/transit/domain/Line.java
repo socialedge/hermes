9ccdb709640f0a2f7b05b00fceddbbb6ab1da810
Illia Ovchynnikov <illia.ovchynnikov@gmail.com>
2017-03-30 17:23:57 +0200

    Respect List order instead of Direction enum

--- backend-transit/src/main/java/eu/socialedge/hermes/backend/transit/domain/Line.java
+++ backend-transit/src/main/java/eu/socialedge/hermes/backend/transit/domain/Line.java
@@ -22,11 +22,12 @@ import org.hibernate.validator.constraints.NotBlank;
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.net.URL;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
-import static java.util.Objects.isNull;
+import static java.util.Objects.nonNull;
 import static org.apache.commons.lang3.Validate.notBlank;
 import static org.apache.commons.lang3.Validate.notNull;
 
@@ -40,9 +41,11 @@ import static org.apache.commons.lang3.Validate.notNull;
 @NoArgsConstructor(force = true, access = AccessLevel.PACKAGE)
 public class Line extends Identifiable<Long>  {
 
+    private static final String DEFAULT_NAME_FORMAT = "%s (%s-%s)";
+
     @Getter
     @Column(name = "code", nullable = false)
-    private @NotBlank String code;
+    private final @NotBlank String code;
 
     @Getter
     @Column(name = "name")
@@ -58,28 +61,43 @@ public class Line extends Identifiable<Long>  {
     private @NotNull Agency agency;
 
     @OneToMany
+    private final @NotNull Station origin;
+
+    @OneToMany
+    private final @NotNull Station destination;
+
+    @OneToMany
     @JoinColumn(name = "line_id")
-    private Set<Route> routes;
+    private Set<Route> routes = new HashSet<>();
 
     @Getter @Setter
     @Column(name = "url")
     private URL url;
 
-    public Line(String code, String name, String description, Agency agency, Set<Route> routes, URL url) {
+    public Line(String code, String name, String description, Station origin, Station destination,
+                Agency agency, Set<Route> routes, URL url) {
         this.code = notBlank(code);
         this.name = notBlank(name);
         this.description = description;
+        this.origin = notNull(origin);
+        this.destination = notNull(destination);
         this.agency = notNull(agency);
-        this.routes = isNull(routes) ? new HashSet<>() : new HashSet<>(routes);
         this.url = url;
+
+        if (nonNull(routes)) {
+            if (!hasGivenEndStations(routes, origin, destination))
+                throw new IllegalArgumentException("Route end station must coincide with given");
+
+            this.routes = new HashSet<>(routes);
+        }
     }
 
-    public Line(String code, String name, Agency agency, Set<Route> routes) {
-        this(code, name, null, agency, routes, null);
+    public Line(String code, Station origin, Station destination, Agency agency, Set<Route> routes) {
+        this(code, defaultName(code, origin, destination), null, origin, destination, agency, routes, null);
     }
 
-    public void code(String code) {
-        this.code = notBlank(code);
+    public Line(String code, Station origin, Station destination, Agency agency) {
+        this(code, origin, destination, agency, null);
     }
 
     public void name(String name) {
@@ -91,7 +109,7 @@ public class Line extends Identifiable<Long>  {
     }
 
     public boolean addRoute(Route route) {
-        return routes.add(route);
+        return hasGivenEndStations(route, origin, destination) && routes.add(route);
     }
 
     public void removeRoute(Route route) {
@@ -100,5 +118,22 @@ public class Line extends Identifiable<Long>  {
 
     public Set<Route> routes() {
         return Collections.unmodifiableSet(routes);
+    }
+
+    private static boolean hasGivenEndStations(Route route, Station origin, Station destination) {
+        val routeStations = route.stations();
+
+        val routeOrigin = routeStations.get(0);
+        val routeDestination = routeStations.get(routeStations.size() - 1);
+
+        return origin.equals(routeOrigin) && destination.equals(routeDestination);
+    }
+
+    private static <T extends Collection<Route>> boolean hasGivenEndStations(T routes, Station origin, Station destination) {
+        return routes.stream().allMatch(r -> hasGivenEndStations(r, origin, destination));
+    }
+
+    private static String defaultName(String code, Station origin, Station destination) {
+        return String.format(DEFAULT_NAME_FORMAT, code, origin.name(), destination.name());
     }
 }
