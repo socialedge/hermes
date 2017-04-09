668848870177b4f52e29965be1f29743a55f52cb
Vladyslav Vlasenko <glasierrvlas@gmail.com>
2017-04-09 20:43:27 +0300

    Implement basic schedule generator (#91)

---
+++ backend-schedule/src/main/java/eu/socialedge/hermes/backend/schedule/domain/BasicScheduleGenerator.java
@@ -0,0 +1,188 @@
+/*
+ * Hermes - The Municipal Transport Timetable System
+ * Copyright (c) 2017 SocialEdge
+ * <p>
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+package eu.socialedge.hermes.backend.schedule.domain;
+
+import eu.socialedge.hermes.backend.schedule.domain.api.ScheduleGenerator;
+import eu.socialedge.hermes.backend.transit.domain.*;
+import lombok.*;
+import lombok.experimental.Accessors;
+import tec.uom.se.unit.Units;
+
+import javax.measure.Quantity;
+import javax.measure.quantity.Length;
+import javax.measure.quantity.Speed;
+import java.time.Duration;
+import java.time.LocalTime;
+import java.util.*;
+
+import static eu.socialedge.hermes.backend.schedule.domain.BasicScheduleGenerator.Direction.INBOUND;
+import static eu.socialedge.hermes.backend.schedule.domain.BasicScheduleGenerator.Direction.OUTBOUND;
+
+@Builder
+@Setter @Accessors(fluent = true)
+public class BasicScheduleGenerator implements ScheduleGenerator {
+
+    private @NonNull String description;
+    private @NonNull Availability availability;
+
+    private @NonNull Route routeInbound;
+    private @NonNull LocalTime startTimeInbound;
+    private @NonNull LocalTime endTimeInbound;
+
+    private @NonNull Route routeOutbound;
+    private @NonNull LocalTime startTimeOutbound;
+    private @NonNull LocalTime endTimeOutbound;
+
+    private @NonNull Duration headway;
+    private @NonNull Duration dwellTime;
+    private @NonNull Quantity<Speed> averageSpeed;
+    private @NonNull Duration minLayover;
+
+    @Override
+    public Schedule generate() {
+        return new Schedule(description, availability, generateTrips());
+    }
+
+    private List<Trip> generateTrips() {
+        val timePoints = generateTimePoints();
+        timePoints.sort(Comparator.comparing(TimePoint::time));
+
+        val trips = new ArrayList<Trip>();
+        for (int vehId = 1; hasNotServicedTimePoints(timePoints); vehId++) {
+            TimePoint startPoint = getNextNotServicedTimePoint(timePoints);
+            trips.addAll(generateVehicleTrips(vehId, startPoint, timePoints));
+        }
+
+        return trips;
+    }
+
+    private List<TimePoint> generateTimePoints() {
+        val timePoints = new ArrayList<TimePoint>();
+        LocalTime nextTimePoint = startTimeInbound;
+        while (nextTimePoint.isBefore(endTimeInbound)) {
+            timePoints.add(new TimePoint(INBOUND, nextTimePoint, false));
+            nextTimePoint = nextTimePoint.plus(headway);
+        }
+        nextTimePoint = startTimeOutbound;
+        while (nextTimePoint.isBefore(endTimeOutbound)) {
+            timePoints.add(new TimePoint(OUTBOUND, nextTimePoint, false));
+            nextTimePoint = nextTimePoint.plus(headway);
+        }
+        return timePoints;
+    }
+
+    private List<Trip> generateVehicleTrips(int vehicleId, TimePoint startPoint, List<TimePoint> timePoints) {
+        val trips = new ArrayList<Trip>();
+
+        while (true) {
+            val trip = generateTrip(vehicleId, startPoint);
+            trips.add(trip);
+
+            //TODO maybe remove last trip and break if its arrival time is after end time? May be some parameter to indicate possible lateness?
+            val currentTime = getArrivalTime(trip);
+            val currentDirection = INBOUND.equals(startPoint.direction()) ? OUTBOUND : INBOUND;
+            val nextPointOpt = findNextNotServicedTimePointAfter(timePoints, currentTime, currentDirection);
+
+            if (nextPointOpt.isPresent()) {
+                startPoint = nextPointOpt.get();
+            } else {
+                break;
+            }
+        }
+
+        return trips;
+    }
+
+    private Trip generateTrip(int vehicleId, TimePoint timePoint) {
+        timePoint.isServiced(true);
+        val route = INBOUND.equals(timePoint.direction()) ? routeInbound : routeOutbound;
+        return new Trip(
+            route,
+            vehicleId,
+            route.stations().get(route.stations().size() - 1).name(),
+            calculateStops(timePoint.time(), route, averageSpeed, dwellTime));
+    }
+
+    private List<Stop> calculateStops(LocalTime startTime, Route route, Quantity<Speed> averageSpeed, Duration dwellTime) {
+        val stops = new ArrayList<Stop>();
+
+        val averageSpeedValue = averageSpeed.to(Units.METRE_PER_SECOND).getValue().longValue();
+
+        for (int i = 0; i < route.stations().size(); i++) {
+            val station = route.stations().get(i);
+            val distTravelled = distanceTraveled(route, station);
+
+            val distValue = distTravelled.to(Units.METRE).getValue().longValue();
+            val arrivalTime = startTime.plusSeconds(distValue / averageSpeedValue).plus(dwellTime.multipliedBy(i));
+
+            stops.add(new Stop(arrivalTime, arrivalTime.plusSeconds(dwellTime.getSeconds()), station));
+        }
+
+        return stops;
+    }
+
+    private boolean isInTimeToTravel(LocalTime from, TimePoint toPoint) {
+        return !Duration.between(from, toPoint.time()).minus(minLayover).isNegative();
+    }
+
+    private static Quantity<Length> distanceTraveled(Route route, Station station) {
+        return route.shape().shapePoints().stream()
+            .filter(shapePoint -> station.location().equals(shapePoint.location()))
+            .findFirst()
+            .map(ShapePoint::distanceTraveled)
+            .orElseThrow(() -> new ScheduleGeneratorException("Could not match stations with route shape. Station not found with location + " + station.location()));
+    }
+
+    private static boolean hasNotServicedTimePoints(List<TimePoint> timePoints) {
+        return timePoints.stream().anyMatch(timePoint -> !timePoint.isServiced());
+    }
+
+    private static TimePoint getNextNotServicedTimePoint(List<TimePoint> timePoints) {
+        return timePoints.stream()
+            .filter(timePoint -> !timePoint.isServiced())
+            .findFirst()
+            .get();
+    }
+
+    private Optional<TimePoint> findNextNotServicedTimePointAfter(List<TimePoint> timePoints, LocalTime time, Direction direction) {
+        return timePoints.stream()
+            .filter(point -> !point.isServiced())
+            .filter(point -> point.direction().equals(direction))
+            .filter(point -> point.time().isAfter(time))
+            .filter(point -> isInTimeToTravel(time, point))
+            .findFirst();
+    }
+
+    private static LocalTime getArrivalTime(Trip trip) {
+        return trip.stops().stream()
+            .max(Comparator.comparing(Stop::arrival))
+            .map(Stop::arrival)
+            .get();
+    }
+
+    enum Direction {
+        INBOUND, OUTBOUND
+    }
+
+    @AllArgsConstructor
+    @Getter @Accessors(fluent = true)
+    private class TimePoint {
+        private Direction direction;
+        private LocalTime time;
+
+        @Setter
+        private boolean isServiced;
+    }
+}
---
+++ backend-schedule/src/test/java/eu/socialedge/hermes/backend/schedule/domain/BasicScheduleGeneratorTest.java
@@ -0,0 +1,112 @@
+/*
+ * Hermes - The Municipal Transport Timetable System
+ * Copyright (c) 2017 SocialEdge
+ * <p>
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+package eu.socialedge.hermes.backend.schedule.domain;
+
+import com.google.gson.*;
+import eu.socialedge.hermes.backend.schedule.domain.api.ScheduleGenerator;
+import eu.socialedge.hermes.backend.transit.domain.Route;
+import eu.socialedge.hermes.backend.transit.domain.Station;
+import eu.socialedge.hermes.backend.transit.domain.Stop;
+import eu.socialedge.hermes.backend.transit.domain.Trip;
+import lombok.val;
+import org.apache.commons.io.IOUtils;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import tec.uom.se.quantity.Quantities;
+
+import javax.measure.Quantity;
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.Arrays;
+import java.util.Collection;
+
+import static java.util.stream.Collectors.toList;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(Parameterized.class)
+public class BasicScheduleGeneratorTest {
+
+    private static final Gson GSON = new GsonBuilder().registerTypeAdapter(Quantity.class, new QuantityTypeAdapter()).create();
+
+    @Parameterized.Parameter
+    public String fileName;
+
+    @Parameterized.Parameters(name = "{index}: {0}")
+    public static Collection<String> data() {
+        File file = new File(BasicScheduleGeneratorTest.class.getClass().getResource("/input").getFile());
+        return Arrays.asList(file.list());
+    }
+
+    @Test
+    public void shouldReturnCorrectSchedule() {
+        ScheduleGenerator generator = GSON.fromJson(readFileAsString("/input/" + fileName), BasicScheduleGenerator.class);
+        val result = generator.generate();
+
+        val expected = GSON.fromJson(readFileAsString("/expectation/" + fileName), Schedule.class);
+
+        assertNotNull(result);
+        assertSchedulesEqual(expected, result);
+    }
+
+    private static void assertSchedulesEqual(Schedule expected, Schedule result) {
+        assertEquals(expected.availability(), result.availability());
+        assertEquals(expected.description(), result.description());
+
+        assertEquals(expected.trips().size(), result.trips().size());
+
+        assertEquals(expected.trips().stream().map(Trip::vehicleId).collect(toList()),
+            result.trips().stream().map(Trip::vehicleId).collect(toList()));
+
+        assertEquals(expected.trips().stream().map(Trip::headsign).collect(toList()),
+            result.trips().stream().map(Trip::headsign).collect(toList()));
+
+        assertEquals(expected.trips().stream().map(Trip::route).map(Route::code).collect(toList()),
+            result.trips().stream().map(Trip::route).map(Route::code).collect(toList()));
+
+        assertEquals(expected.trips().stream().map(Trip::stops).collect(toList()),
+            result.trips().stream().map(Trip::stops).collect(toList()));
+
+        assertEquals(expected.trips().stream()
+                .map(trip -> trip.stops().stream().map(Stop::station).map(Station::name).collect(toList()))
+                .collect(toList()),
+            result.trips().stream()
+                .map(trip -> trip.stops().stream().map(Stop::station).map(Station::name).collect(toList()))
+                .collect(toList()));
+    }
+
+    private static String readFileAsString(String path) {
+        try {
+            return IOUtils.toString(BasicScheduleGeneratorTest.class.getResourceAsStream(path));
+        } catch (IOException e) {
+            throw new RuntimeException("Exception while reading file: " + path, e);
+        }
+    }
+}
+
+class QuantityTypeAdapter implements JsonSerializer<Quantity>, JsonDeserializer<Quantity> {
+
+    @Override
+    public Quantity deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+        return Quantities.getQuantity(json.getAsJsonPrimitive().getAsString());
+    }
+
+    @Override
+    public JsonElement serialize(Quantity src, Type typeOfSrc, JsonSerializationContext context) {
+        return new JsonPrimitive(src.toString());
+    }
+}
