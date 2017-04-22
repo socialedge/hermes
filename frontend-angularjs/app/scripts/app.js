c833d190cd6e4898d7644dd09c889b2a8c2dd266
Illia Ovchynnikov <illia.ovchynnikov@gmail.com>
2017-04-22 17:38:01 +0200

    Put frontend html mockup on Angular

--- frontend-angularjs/app/scripts/app.js
+++ frontend-angularjs/app/scripts/app.js
@@ -2,12 +2,33 @@
 
 var $angular = angular.module('hermesApp', ['ngAnimate', 'ngCookies', 'ngResource', 'ngRoute', 'ngSanitize', 'ngTouch']);
 
-$angular.config(function ($routeProvider) {
+$angular.config(function ($routeProvider, $locationProvider) {
+  $locationProvider.hashPrefix('');
+
   $routeProvider
     .when('/', {
       templateUrl: '../views/index.html',
-      controller: 'IndexCtrl',
-      controllerAs: 'index'
+      controller: 'IndexCtrl'
+    })
+    .when('/lines', {
+      templateUrl: '../views/lines.html',
+      controller: 'LinesCtrl',
+      controllerAs: 'lines'
+    })
+    .when('/schedules', {
+      templateUrl: '../views/schedules.html',
+      controller: 'SchedulesCtrl',
+      controllerAs: 'schedules'
+    })
+    .when('/schedules/:scheduleId', {
+      templateUrl: '../views/schedule.html',
+      controller: 'ScheduleCtrl',
+      controllerAs: 'schedule'
+    })
+    .when('/stations', {
+      templateUrl: '../views/stations.html',
+      controller: 'StationsCtrl',
+      controllerAs: 'stations'
     })
     .otherwise({
       redirectTo: '/'
