dacfcd2d73a9a6763487be795c8b5c813c8a5440
Vladyslav Vlasenko <glasierrvlas@gmail.com>
2017-05-15 23:12:48 +0300

    Create view for single schedule (#117)

---
+++ backend-application/src/main/java/eu/socialedge/hermes/backend/application/api/projection/RichLineProjection.java
@@ -0,0 +1,36 @@
+/*
+* Hermes - The Municipal Transport Timetable System
+* Copyright (c) 2017 SocialEdge
+* <p>
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* <p>
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*/
+package eu.socialedge.hermes.backend.application.api.projection;
+
+import eu.socialedge.hermes.backend.transit.domain.VehicleType;
+
+import java.net.URL;
+import java.util.List;
+
+public interface RichLineProjection {
+    String getCode();
+
+    String getName();
+
+    String getDescription();
+
+    AgencyProjection getAgency();
+
+    RichRouteProjection getInboundRoute();
+
+    RichRouteProjection getOutboundRoute();
+
+    URL getUrl();
+}
--- frontend-angularjs/app/scripts/controllers/scheduleCtrl.js
+++ frontend-angularjs/app/scripts/controllers/scheduleCtrl.js
@@ -1,3 +1,35 @@
 'use strict';
 
-angular.module('hermesApp').controller('ScheduleCtrl', function () {});
+angular.module('hermesApp').controller('ScheduleCtrl', function ($scope, $http, $routeParams, $location, env) {
+
+  function fetchSchedule(url, callback) {
+    $http.get(url + "?projection=richScheduleProjection")
+      .then(function(result) {
+        callback(result.data);
+      });
+  }
+
+  $scope.loadPage = function(callback) {
+    if (!$routeParams.show) {
+      $location.path("/schedules");
+    }
+    $scope.page = {};
+    fetchSchedule($routeParams.show, function(response) {
+      $scope.page.lineCode = response.line.code;
+      $scope.page.inboundRouteCode = response.line.inboundRoute.code;
+      $scope.page.outboundRouteCode = response.line.outboundRoute.code;
+      $scope.page.inboundStations = response.line.inboundRoute.stations;
+      $scope.page.outboundStations = response.line.outboundRoute.stations;
+      $scope.page.inboundTrips = response.trips.filter(function(trip) {
+        return trip.route.code === response.line.inboundRoute.code;
+      });
+      $scope.page.outboundTrips = response.trips.filter(function(trip) {
+        return trip.route.code === response.line.outboundRoute.code;
+      });
+
+      if (typeof callback === "function")
+        callback($scope.page);
+    });
+  };
+
+});
