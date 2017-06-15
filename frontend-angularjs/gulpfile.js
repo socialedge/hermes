77d639458fb8c48ffde236e23718f17f11ac1afb
Illia Ovchynnikov <illia.ovchynnikov@gmail.com>
2017-06-15 08:34:13 +0200

    Enable backend url parametrization

--- frontend-angularjs/gulpfile.js
+++ frontend-angularjs/gulpfile.js
@@ -4,33 +4,61 @@ var gulp = require("gulp"),
     mainBowerFiles = require('main-bower-files'),
     angularFilesort = require('gulp-angular-filesort'),
     webserver = require('gulp-webserver'),
-    runSequence = require('run-sequence');
+    runSequence = require('run-sequence'),
+    envify = require('gulp-envify'),
+    args = require('yargs').argv,
+    es = require('event-stream'),
+    clean = require('gulp-clean');
 
-gulp.task('inject', function () {
-  return gulp.src('./app/app.html')
-    // Bower dependencies
-    .pipe(inject(gulp.src(mainBowerFiles()), {relative: true, name: 'bower'}))
+const BUILD_DEST = './build/';
+const BACKEND_DEFAULT = "http://localhost:8080";
+const SERVE_PORT_DEFAULT = "8000";
 
-    // Application dependencies
-    .pipe(inject(gulp.src('./app/styles/**/*.css'), {relative: true, name: 'app'}))
-    .pipe(inject(gulp.src('./app/scripts/**/*.js').pipe(angularFilesort()), {relative: true, name: 'app'}))
-
-    .pipe(rename('index.html'))
-    .pipe(gulp.dest('./app/'));
+gulp.task('clean', function () {
+  return gulp.src(BUILD_DEST, {read: false}).pipe(clean());
 });
 
-gulp.task('webserver', function () {
+gulp.task('build', function () {
+  var backendUrl = args.backend || BACKEND_DEFAULT;
+
+  return es.merge(
+    // Index.html
+    gulp.src('./app/index.html')
+      // Bower dependencies
+      .pipe(inject(gulp.src(mainBowerFiles()), {relative: true, name: 'bower'}))
+
+      // Application dependencies
+      .pipe(inject(gulp.src('./app/styles/**/*.css'), {relative: true, name: 'app'}))
+      .pipe(inject(gulp.src('./app/scripts/**/*.js').pipe(angularFilesort()), {relative: true, name: 'app'}))
+
+      .pipe(rename('index.html'))
+      .pipe(gulp.dest(BUILD_DEST)),
+
+    gulp.src('./app/scripts/app.js')
+      .pipe(envify({
+        backendBaseUrl: backendUrl
+      }))
+      .pipe(gulp.dest(BUILD_DEST + 'scripts/')),
+
+    gulp.src(['./app/**/*', '!./app/index.html', '!./app/scripts/app.js'])
+      .pipe(gulp.dest(BUILD_DEST))
+  );
+});
+
+gulp.task('serve', function () {
+  var servePort = args.port || SERVE_PORT_DEFAULT;
   return gulp
-    .src('./app/')
+    .src(BUILD_DEST)
     .pipe(webserver({
+      port: servePort,
       livereload: true,
       directoryListing: false,
       fallback: 'index.html'
     }));
 });
 
-gulp.task('serve', function () {
-  runSequence('inject', 'webserver');
+gulp.task('deploy', function () {
+  runSequence('clean', 'build', 'serve');
 });
-gulp.task('index', ['serve']);
-gulp.task('run', ['serve']);
+
+gulp.task('index', ['deploy']);
