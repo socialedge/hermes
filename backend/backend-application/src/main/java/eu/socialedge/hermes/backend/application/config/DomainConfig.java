1e72d735c8bdd299afa59cc47a76dbb78adfa861
Illia Ovchynnikov <illia.ovchynnikov@gmail.com>
2017-06-19 15:49:02 +0200

    Add temporary agency init for embedded mongodb profile

--- backend/backend-application/src/main/java/eu/socialedge/hermes/backend/application/config/DomainConfig.java
+++ backend/backend-application/src/main/java/eu/socialedge/hermes/backend/application/config/DomainConfig.java
@@ -14,14 +14,23 @@
 */
 package eu.socialedge.hermes.backend.application.config;
 
+import com.neovisionaries.i18n.LanguageCode;
 import eu.socialedge.hermes.backend.schedule.infrasturcture.config.ScheduleConfiguration;
+import eu.socialedge.hermes.backend.transit.domain.Agency;
 import eu.socialedge.hermes.backend.transit.domain.GoogleMapsShapeFactory;
 import eu.socialedge.hermes.backend.transit.domain.ShapeFactory;
+import eu.socialedge.hermes.backend.transit.domain.repository.AgencyRepository;
 import eu.socialedge.hermes.backend.transit.infrastructire.config.TransitConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Profile;
+
+import javax.annotation.PostConstruct;
+import java.net.URL;
+import java.util.TimeZone;
 
 @Configuration
 @Import({TransitConfiguration.class, ScheduleConfiguration.class})
@@ -31,5 +40,33 @@ public class DomainConfig {
     @Value("${ext.google-maps.api}")
     public ShapeFactory shapeFactory(String apiKey) {
         return new GoogleMapsShapeFactory(apiKey);
+    }
+
+    @Deprecated
+    @Configuration @Profile("dev")
+    public static class InitDevAgency {
+
+        @Autowired
+        AgencyRepository agencyRepository;
+
+        @Value("${domain.agency.name}")
+        String name;
+
+        @Value("${domain.agency.lang}")
+        String lang;
+
+        @Value("${domain.agency.phone}")
+        String phone;
+
+        @Value("${domain.agency.timeZone}")
+        String timeZone;
+
+        @Value("${domain.agency.url}")
+        URL url;
+
+        @PostConstruct
+        void init() {
+            agencyRepository.save(new Agency(name, LanguageCode.getByCode(lang), phone, TimeZone.getTimeZone(timeZone), url));
+        }
     }
 }
