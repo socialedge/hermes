8be85a60d02bc9bc74917cc1f804d4a1a80f1862
Illia Ovchynnikov <illia.ovchynnikov@gmail.com>
2017-10-14 19:02:58 +0200

    Simplify EntityBuilder util proxy menthods

--- backend/backend-application/src/main/java/eu/socialedge/hermes/backend/application/api/v2/mapping/util/EntityBuilder.java
+++ backend/backend-application/src/main/java/eu/socialedge/hermes/backend/application/api/v2/mapping/util/EntityBuilder.java
@@ -18,30 +18,25 @@ import static org.apache.commons.lang3.Validate.notNull;
  */
 public final class EntityBuilder<T> {
 
+    private static final String DEFAULT_ID_FILED_NAME = "id";
+
     private Class<T> entityClass;
-    private String idFieldName = "id";
+    private String idFieldName = DEFAULT_ID_FILED_NAME;
     private Object idValue;
 
+    @Deprecated
     public EntityBuilder(Class<T> entityClass) {
         this.entityClass = notNull(entityClass);
     }
 
+    @Deprecated
     public static <T> EntityBuilder<T> of(Class<T> clazz) {
         return new EntityBuilder<>(clazz);
     }
 
-    public EntityBuilder<T> idField(String name) {
-        this.idFieldName = name;
-        return this;
-    }
-
-    public EntityBuilder<T> idValue(Object value) {
-        this.idValue = value;
-        return this;
-    }
-
-    public T build() throws ReflectiveOperationException {
-        val defConstructor = entityClass.getDeclaredConstructor();
+    public static <T> T proxy(Class<T> clazz, String idFieldName, Object idValue)
+            throws ReflectiveOperationException {
+        val defConstructor = clazz.getDeclaredConstructor();
         defConstructor.setAccessible(true);
         T instance = defConstructor.newInstance();
 
@@ -50,5 +45,26 @@ public final class EntityBuilder<T> {
         idField.set(instance, idValue);
 
         return instance;
+    }
+
+    public static <T> T proxy(Class<T> clazz, Object idValue) throws ReflectiveOperationException {
+        return proxy(clazz, DEFAULT_ID_FILED_NAME, idValue);
+    }
+
+    @Deprecated
+    public EntityBuilder<T> idField(String name) {
+        this.idFieldName = name;
+        return this;
+    }
+
+    @Deprecated
+    public EntityBuilder<T> idValue(Object value) {
+        this.idValue = value;
+        return this;
+    }
+
+    @Deprecated
+    public T build() throws ReflectiveOperationException {
+        return proxy(entityClass, idFieldName, idValue);
     }
 }
