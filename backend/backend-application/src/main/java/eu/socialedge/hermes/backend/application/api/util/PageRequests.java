c0e282717cd245dd71d822ffbd9ffb540ba3e3dc
Illia Ovchynnikov <illia.ovchynnikov@gmail.com>
2017-10-14 20:25:24 +0200

    Add missed Copyrights file headers

--- backend/backend-application/src/main/java/eu/socialedge/hermes/backend/application/api/util/PageRequests.java
+++ backend/backend-application/src/main/java/eu/socialedge/hermes/backend/application/api/util/PageRequests.java
@@ -1,3 +1,18 @@
+/*
+ * Hermes - The Municipal Transport Timetable System
+ * Copyright (c) 2016-2017 SocialEdge
+ * <p>
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
 package eu.socialedge.hermes.backend.application.api.util;
 
 import lombok.experimental.var;
