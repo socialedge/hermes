07d7b6bb72c2b1f1d28e67cf841c5da4cc95a060
Illia Ovchynnikov <illia.ovchynnikov@gmail.com>
2017-10-15 22:55:23 +0200

    Enable swagger client on frontend-v2

--- frontend-v2/src/app.js
+++ frontend-v2/src/app.js
@@ -13,9 +13,10 @@ import './styles/main.css';
 import 'angular-material/angular-material.css';
 
 import appConfigEnv from './app.config.env';
-import appRouteI18n from './app.config.i18n';
-import appRouteRoute from './app.config.route';
-import appRouteStyle from './app.config.style';
+import appConfigI18n from './app.config.i18n';
+import appConfigRoute from './app.config.route';
+import appConfigStyle from './app.config.style';
+import appConfigSwagger from './app.config.swagger';
 
 import AppContent from './components/app-content/app-content';
 
@@ -32,8 +33,8 @@ export default angular.module('hermes-frontend-v2', [
 ])
   .constant('ENV', process.env.ENV_NAME)
   .config(appConfigEnv)
-  .config(appRouteI18n)
-  .config(appRouteRoute)
-  .config(appRouteStyle)
-
+  .config(appConfigI18n)
+  .config(appConfigRoute)
+  .config(appConfigStyle)
+  .config(appConfigSwagger)
   .name;
