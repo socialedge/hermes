591360cfc8464050664deb98af1f160b0f2c7b84
Illia Ovchynnikov <illia.ovchynnikov@gmail.com>
2017-12-13 16:46:36 +0100

    Commit smths

--- frontend-v2/src/pages/lines-page/lines-page.controller.js
+++ frontend-v2/src/pages/lines-page/lines-page.controller.js
@@ -1,8 +1,26 @@
 import './lines-page.style.css';
+import RecordManagementController from "../RecordManagementController";
+import PopupService from "../../services/popup/popup.service";
 
-class LinesPageController {
+const DISABLE_PAGING = true;
 
-  constructor() {
+class LinesPageController extends RecordManagementController {
+
+  constructor($scope, $timeout, $mdBottomSheet, backend, popupService) {
+    super($scope, $timeout, $mdBottomSheet, null, null, popupService, DISABLE_PAGING);
+    this.backend = backend;
+  }
+
+  async $deleteRecord(id) {
+    return (await this.backend).apis.lines.deleteLine({id: id});
+  }
+
+  async $fetchRecords(params) {
+    return (await (await this.backend).apis.lines.listLines(params)).body;
+  }
+
+  static get $inject() {
+    return ['$scope', '$timeout', '$mdBottomSheet', 'backend', PopupService.name];
   }
 }
 
